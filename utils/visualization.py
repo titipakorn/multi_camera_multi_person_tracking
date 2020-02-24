5dfc9e4d52da943ebd7941b493b75574bf7d6696
Prince <prince@Princes-MacBook-Pro.local>
2020-02-24 16:50:11 +0700

    update vis

--- utils/visualization.py
+++ utils/visualization.py
@@ -19,7 +19,7 @@ from mc_tracker.sct import SingleCameraTracker
 
 def draw_detections(frame, detections):
     """Draws detections and labels"""
-    cv.putText(frame, 'IN: {}, OUT: {}'.format(SingleCameraTracker.COUNT_IN,SingleCameraTracker.COUNT_OUT), (5, 10), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv.putText(frame, 'IN: {}, OUT: {}'.format(SingleCameraTracker.COUNT_IN,SingleCameraTracker.COUNT_OUT), (5, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     for i, obj in enumerate(detections):
         left, top, right, bottom = obj.rect
         label = obj.label
@@ -50,5 +50,5 @@ def visualize_multicam_detections(frames, all_objects, fps=''):
     label_size, base_line = cv.getTextSize(str(fps),
                                            cv.FONT_HERSHEY_SIMPLEX, 1, 2)
     cv.putText(vis, str(fps), (base_line*2, base_line*3),
-               cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+               cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     return vis
